16bdfa2e9a9c72059e488385644948d23decda4b
Roumwelle Sta. Ines <roumwelle@gmail.com>
2014-07-24 09:42:09 -0400

    Set up browser live reload

--- gruntfile.js
+++ gruntfile.js
@@ -1,23 +1,34 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    express: {
+      all: {
+        options: {
+          port: 9000,
+          hostname: 'localhost',
+          bases: ['www'],
+          livereload: true
+        }
+      }
+    },
     less: {
       development: {
         options: {
-          paths: ["src/less"],
+          paths: ['src/less'],
         },
         files: {
-          "www/css/styles.css": "src/less/*.less"
+          'www/css/styles.css': 'src/less/*.less'
         }
       }
     },
     watch: {
-      files: "src/less/*.less",
-      tasks: ["less"]
+      files: 'src/less/*.less',
+      tasks: ['less']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express');
 
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('server', ['express', 'watch']);
 };
